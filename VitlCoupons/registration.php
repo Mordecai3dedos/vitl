83dc9fc6a2e7809c928e222b1a7b72b0dc5eb71c
David Morales Cabello <david@santafixie.com>
2023-02-07 23:38:36 +0100

    Module registration and admin file created

---
+++ VitlCoupons/registration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'DavidMorales_VitlCoupons',
+    __DIR__,
+);
